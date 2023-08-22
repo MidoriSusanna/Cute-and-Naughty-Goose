64b57cfe921a2ccb1cc50c66cc6e7830921b0947
Susanna Legnani <susi@MacBook-Air.local>
2023-08-22 08:50:55 +0200

    Set start and next question function. Show question function, reset state function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,63 @@
 // Tag the Start Button and add event listener on the click of the user to start the game
-let startButton = document.getElementById('start-btn')
-startButton.addEventListener('click', runGame)
+let startButton = document.getElementById('start-btn');
+startButton.addEventListener('click', runGame);
 
-let questionContainerElement = document.getElementById('question-answers-container')
+let nextButton = document.getElementById('next-btn');
 
-// When the game starts, the start button needs to hide adn questions and options displayed
+let questionContainerElement = document.getElementById('question-answers-container');
+
+// Will be defined in the setNextQuestion function
+let currentQuestionIndex;
+
+let questionElement = document.getElementById('questions');
+let answerButtonsElement = document.getElementById('answer-buttons');
+
+// When the game starts, the start button needs to be hidden, questions and options displayed
 function runGame() {
-    startButton.classList.add('hide')
-    questionContainerElement.classList.remove('hide')
-    setNextQuestion()
+    startButton.classList.add('hide');
+    questionContainerElement.classList.remove('hide');
+    currentQuestionIndex = 0;
+    setNextQuestion();
 
 }
 
 function setNextQuestion () {
-
+    if (currentQuestionIndex < questions.length) {
+        let question = questions[currentQuestionIndex];
+        showQuestion(question);
+        currentQuestionIndex++;
+    } else {
+        // Go to profile according to the score
+    }
 }
 
-function selectAnswer(answer) {
+function showQuestion() {
+    questionElement.innerText = currentQuestion.question;
+    // Clear previous answer buttons
+    answerButtonsElement.innerHTML = '';
+
+    // Iterates through the array of answer choices associated with the current question
+    currentQuestion.answers.forEach(answer => {
+        let button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
+        // Adds an event listener to the button
+        button.addEventListener('click', () => selectAnswer(answer));
+        answerButtonsElement.appendChild(button);
+    });
+}
+
+// Reset state after answering the question
+function resetState() {
+    nextButton.classList.add('hide');
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild);
+    }
+}
+
+function selectAnswer() {
+
 
 }
 
@@ -32,20 +73,19 @@ function selectProfile() {
 
 }
 
-let questionNodes = [
+// Array of questions
+let questions = [
     {
-        id: 1,
         question: "As you awaken in the morning, a profound weariness envelops you. However, duty calls, and you must head to the pond for work. You make your way to the kitchen to prepare your breakfast of corn and salad, only to find that your chameleon roommate has neglected the dishes, leaving you with the task. The chameleon has expertly blended into its surroundings. What do you do?",
-        options: [
-            { answer: "Duck the chameleon. You're going to hunt in the pond. Before leaving, you decide to play a little prank on your elusive chameleon roommate. You sneakily hide its alarm clock (hoping that it's not observing your actions from its clever hiding place).",
-              nextQuestion: 2,
+        answers: [
+            { text: "Duck the chameleon. You're going to hunt in the pond. Before leaving, you decide to play a little prank on your elusive chameleon roommate. You sneakily hide its alarm clock (hoping that it's not observing your actions from its clever hiding place).",
+              cute: false,
             },
-            { answer: "I'm certain that the chameleon must have had a valid reason for not washing the dishes—perhaps it's feeling unwell or exhausted. I'll tidy up using my quills, and then I'll go to the pond for my work.",
-              nextQuestion: 2,
+            { text: "I'm certain that the chameleon must have had a valid reason for not washing the dishes—perhaps it's feeling unwell or exhausted. I'll tidy up using my quills, and then I'll go to the pond for my work.",
+              cute: true,
             }
-
         ]
     }
-]
+];
 
-runGame()
+runGame();
