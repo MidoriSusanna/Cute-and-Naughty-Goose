ba36868ffa4610b99de81ca04f9f170f9338e3cd
Susanna Legnani <susi@MacBook-Air.local>
2023-08-24 09:50:25 +0200

    Move the displayGooseName function to the profile.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -118,15 +118,3 @@ function resetScore() {
 });
 }
 resetScore();
-
-// Function to display the Goose name after submitting it, display as a child of the submit element
-function displayGooseName(e) { 
-    let form = document.querySelector('form');
-    let postGooseName = document.createElement('h1');
-    
-    form.addEventListener('submit', (e) => {
-    e.preventDefault(); // The e.preventDefault() is to prevent the default settings of the submit EventListener: https://betterprogramming.pub/click-vs-submit-eventlisteners-536b62be9359
-    postGooseName.innerText = `Hi, ${gooseName}!`;
-    document.getElementById("submit-btn").appendChild(postGooseName);
-})};
-displayGooseName();
