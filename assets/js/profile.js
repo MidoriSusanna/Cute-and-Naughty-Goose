33ed666ee48539abcd71a6cdfbd712c82ee4d2ae
Susanna Legnani <susi@MacBook-Air.local>
2023-08-24 12:25:09 +0200

    Fix bug of the displayGooseName function by adding a variable and appendChild to a new html element

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -4,8 +4,10 @@ function displayGooseName(e) {
     let postGooseName = document.createElement('h1');
     
     form.addEventListener('submit', (e) => {
+    let getName = document.getElementById('gooseName').value;
+    console.log(getName); // Check if the name is printed to the console
     e.preventDefault(); // The e.preventDefault() is to prevent the default settings of the submit EventListener: https://betterprogramming.pub/click-vs-submit-eventlisteners-536b62be9359
-    postGooseName.innerText = `Hi, ${gooseName}!`;
-    document.getElementById("submit-btn").appendChild(postGooseName);
+    postGooseName.innerText = `Hi, ${getName}!`;
+    document.getElementById("displayName").appendChild(postGooseName);
 })};
 displayGooseName();
