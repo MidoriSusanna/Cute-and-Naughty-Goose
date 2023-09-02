d27f378c4f2c5a923933646046e219cb36709b2b
Susanna Legnani <susi@MacBook-Air.station>
2023-09-02 12:04:55 +0200

    Semplification to the profile.js

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -2,13 +2,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const cuteScore = JSON.parse(localStorage.getItem('cute-goose-score'));
     const naughtyScore = JSON.parse(localStorage.getItem('naughty-goose-score'));
-    
-    // If no score is found, go back to index page 
-    if (cuteScore && naughtyScore) {
-        document.getElementById('cute-goose-score').textContent = cuteScore;
-        document.getElementById('naughty-goose-score').textContent = naughtyScore;
-    }
-    else {
-        window.location.href = './index.html'
-    }
+
+    document.getElementById('cute-goose-score').textContent = cuteScore;
+    document.getElementById('naughty-goose-score').textContent = naughtyScore;
 });
